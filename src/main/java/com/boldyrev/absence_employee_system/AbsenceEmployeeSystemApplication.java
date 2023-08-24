c103f563762d3cb0cb32fb18e0f2775bdae63e1a
Alex Boldyrev <alexandrboldyrev08@gmail.com>
2023-08-24 14:37:36 +0300

    init project structure

---
+++ src/main/java/com/boldyrev/absence_employee_system/AbsenceEmployeeSystemApplication.java
@@ -0,0 +1,13 @@
+package com.boldyrev.absence_employee_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbsenceEmployeeSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AbsenceEmployeeSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/boldyrev/absence_employee_system/TestAbsenceEmployeeSystemApplication.java
@@ -0,0 +1,24 @@
+package com.boldyrev.absence_employee_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.context.annotation.Bean;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.utility.DockerImageName;
+
+@TestConfiguration(proxyBeanMethods = false)
+public class TestAbsenceEmployeeSystemApplication {
+
+    @Bean
+    @ServiceConnection
+    PostgreSQLContainer<?> postgresContainer() {
+        return new PostgreSQLContainer<>(DockerImageName.parse("postgres:latest"));
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.from(AbsenceEmployeeSystemApplication::main)
+            .with(TestAbsenceEmployeeSystemApplication.class).run(args);
+    }
+
+}
